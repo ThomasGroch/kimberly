2bb78d13f05ee27c30ca0814a52e15c61c98c2f8
Alface <thomas@MacBook-Air-de-Thomas-2.local>
2015-06-19 13:49:13 -0300

    update na funcao testHeader

--- util.php
+++ util.php
@@ -31,11 +31,25 @@ function testHeader($url) {
 	
 	$headers = get_headers($url, 1);
 	if (strpos($headers[0], '404') ){
-		$logger->info('Link > '.$url.' >'. $headers[0]);
+		$logger->info('[Skip] Respota 404 > '.$url);
+		return false;
+	}
+	if( ! is_array($headers['Location']) {
+		$logger->info('[Skip] Link do Zanox corrompido > '.$headers['Location']);
 		return false;
 	}
 	$link_do_produto = $headers['Location']['0'];
 	//$logger->info('locato:'.$headers['Location']['0']);
 	return $link_do_produto;
+}
+function recursive_unset(&$array, $unwanted_key) {
+	if( isset($array[$unwanted_key]) ){
+    	unset($array[$unwanted_key]);
+	}
+    foreach ($array as &$value) {
+        if (is_array($value)) {
+            recursive_unset($value, $unwanted_key);
+        }
+    }
 }
 ?>
