dfc1764abfe8440ae2b1537764b85fe03b578cff
Jeam Alves <jeam@tagbox.com.br>
2015-06-19 17:12:08 -0300

    Método para buscar cor, tamanho finalizado

--- padroes/Olook.php
+++ padroes/Olook.php
@@ -16,24 +16,46 @@
 		* Saida: array de um produto + campos extras
 		*/
 		public function prepare(){
+
 			$html = parent::prepare();
+
+			//Verifica se o produto está indisponível.
+			if($html->find('p.out')){ 
+				return false;
+			}
+
+			// Obtem tamanho
+			$this->produto['tamanho'] = '';
+			$tamanhos = '';
+			foreach($html->find('div.size', 0)->find('label') as $tamanho){
+				$tamanhos .= $tamanho->plaintext.'|';
+			}
+
+			// Remove o ultimo |
+			$tamanhos = substr($tamanhos, 0, -1);
+			// Remove dublicados
+			$tamanhos = explode('|', $tamanhos);
+			$tamanhos = array_unique($tamanhos);
+			$this->produto['tamanho'] = implode('|', $tamanhos);
 
 
 			// Obtem cor
 			$i=0;
 			$this->produto['cor'] = '';
-			foreach($html->find('div.colors') as $selecao){
-				if($i==0){
-					foreach($selecao->find('img.color-option') as $cor){
-						$this->produto['cor'] .= $cor->title.'|';
-					}
-				}
-				$i++;
+			
+			foreach($html->find('ol.colors', 0)->find('img')  as $cor){
+				
+				$this->produto['cor'] .= $cor->title.'|';	
+				
 			}
 
-			echo '<pre>';print_r($this->produto);
+			// Remove o ultimo |
+			$this->produto['cor'] = substr($this->produto['cor'], 0, -1);
+			// Obtem marca
+			$this->produto['marca'] = 'Olook';
 
-
+			// Produto Tratado com sucesso
+			return true;
 		}
 
 	}
