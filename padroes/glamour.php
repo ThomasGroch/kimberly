230f4653c1dd449152fbc9d837e7b9ed2f758b9e
Alface <thomas@MacBook-Air-de-Thomas-2.local>
2015-06-24 10:48:12 -0300

    Glamour tamanho opcional

--- padroes/glamour.php
+++ padroes/glamour.php
@@ -10,12 +10,6 @@
 			parent::__construct($array);
 		}
 
-		public function set_produto($produto){
-			$link_arrumado = str_replace('asd', replace, $this->produto['trackingLinks']['trackingLink']['ppc']);
-			$this->produto['trackingLinks']['trackingLink']['ppc'] = $link_arrumado;
-			$this->produto = $produto;
-		}
-
 		/*
 		* Se for um produto que interessar a tagbox, 
 		* devera retornar true
@@ -42,24 +36,6 @@
 		public function prepare(){
 			$html = parent::prepare();
 			
-			// Obtem tamanho
-			$tamanho_script = $html->find('head',0);
-			if( empty($tamanho_script)){
-			 	$this->logger->info('[Skip] Nao foi foi possivel encontrar tamanhos');
-			 	return false;
-			}
-			$json = $tamanho_script->last_child();
-			$json = get_string_between($json, '<script>var skuJson_0 = ', ';CATALOG_SDK.');
-			$json = json_decode($json, true);
-
-			foreach ($json['skus'] as $key => $sku) {
-				if( $sku['available'] ){
-					$tamanhos[] = $sku['dimensions']['Tamanho'];
-				}
-			}
-			$this->produto['tamanho'] = implode('|', $tamanhos);
-
-
 			// Obtem marca
 			$marca = trim($html->find('p.brand', 0)->find('a',0)->plaintext);
 			if( empty($marca)){
@@ -67,6 +43,24 @@
 			 	return false;
 			}
 			$this->produto['marca'] = $marca;
+			
+			// Obtem tamanho
+			$tamanho_script = $html->find('head',0);
+			if( ! empty($tamanho_script)){
+				$json = $tamanho_script->last_child();
+				$json = get_string_between($json, '<script>var skuJson_0 = ', ';CATALOG_SDK.');
+				$json = json_decode($json, true);
+
+				foreach ($json['skus'] as $key => $sku) {
+					if( $sku['available'] ){
+						$tamanhos[] = $sku['dimensions']['Tamanho'];
+					}
+				}
+				$this->produto['tamanho'] = implode('|', $tamanhos);
+			}
+			// Se nao encontrar um tamanho coloca como tamanho unico
+			$this->produto['tamanho'] = ( empty($this->produto['tamanho']) ) ? 'U' : $this->produto['tamanho'];
+
 
 			// Produto Tratado com sucesso
 			return true;
