9861fc41f1a1d14fd6f1ceaefe1a1e4f0ccc4577
Jeam Alves <jeanmalves@gmail.com>
2015-06-26 16:41:42 -0300

    Adição de nova loja.

---
+++ padroes/marcyn.php
@@ -0,0 +1,18 @@
+<?php
+	require __DIR__ . '/afilio.php';
+
+	class Marcyn extends Afilio {
+
+		var $xml_url = 'http://v2.afilio.com.br/aff/aff_get_boutique.php?boutiqueid=39281-895842&token=53e355b5e465b0.28149070&progid=1180&format=XML';
+
+		var $produto = array();
+
+		var $white_list_category = array();
+		
+		var $black_list_category = array();
+
+
+		public function prepare(){
+			parent::prepare();
+		}
+	}
