cd60e872e37522efc98eb0d6384c1e4b80a91dbc
Alface <thomas@MacBook-Air-de-Thomas-2.local>
2015-07-01 14:49:26 -0300

    json decode sem aspas

--- util.php
+++ util.php
@@ -124,4 +124,16 @@ function get_string_between($string, $start, $end='', $numero = 1){
         return $resultado;
     }   
 
+function my_json_decode($s) {
+    $s = str_replace(
+        array('"',  "'"),
+        array('\"', '"'),
+        $s
+    );
+    $s = preg_replace('/(\w+):/i', '"\1":', $s);
+    //echo sprintf('{%s}', $s);exit;
+    $res = preg_replace('/(\w+):/i', '"\1":', $s);
+    return json_decode($res, true);
+}
+
 ?>
